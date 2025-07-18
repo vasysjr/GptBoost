f8d10d93aba07dcb0c9bd8b94220f1e426a8a38d
vasysjr <iiichupiii@gmail.com>
2025-07-18 21:20:45 +0300

    Update route.ts

--- app/api/generate-image/route.ts
+++ app/api/generate-image/route.ts
@@ -11,7 +11,7 @@ export async function POST(req: Request) {
 
   try {
     const response = await fetch(
-      "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-3-medium-diffusers",
+      "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
       {
         method: "POST",
         headers: {
